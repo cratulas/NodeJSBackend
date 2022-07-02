c120f58cae9647d99b94001b69371093658f7a69
cratulas <mauricios.mora@gmail.com>
2022-07-02 21:47:37 +0100

    subir a heroku

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -5,17 +5,21 @@ const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
 
 
-const usuariosGet = (req = request, res = response) => {
+const usuariosGet = async(req = request, res = response) => {
 
-    const { q, nombre = 'No name', apikey, page = 1, limit } = req.query;
+    const { limite = 5, desde = 0 } = req.query;
+    const query = { estado: true };
+
+    const [ total, usuarios ] = await Promise.all([
+        Usuario.countDocuments(query),
+        Usuario.find(query)
+            .skip( Number( desde ) )
+            .limit(Number( limite ))
+    ]);
 
     res.json({
-        msg: 'get API - controlador',
-        q,
-        nombre,
-        apikey,
-        page, 
-        limit
+        total,
+        usuarios
     });
 }
 
@@ -25,14 +29,6 @@ const usuariosPost = async(req, res = response) => {
     const { nombre, correo, password, rol } = req.body;
     const usuario = new Usuario({ nombre, correo, password, rol });
 
-
-    // Verificar si el correo existe
-    const existeEmail = await Usuario.findOne({ correo });
-    if ( existeEmail ) {
-        return res.status(400).json({
-            msg: 'Ese correo ya se encuentra en la DB'
-        })
-    }
 
     // Encriptar la contraseña
     const salt = bcryptjs.genSaltSync();
@@ -47,15 +43,22 @@ const usuariosPost = async(req, res = response) => {
     });
 }
 
-const usuariosPut = (req, res = response) => {
+const usuariosPut = async(req, res = response) => {
 
     const { id } = req.params;
+    const { _id, password, google, ...resto } = req.body;
 
-    res.json({
-        msg: 'put API - usuariosPut',
-        id
-    });
+    if ( password ) {
+        // Encriptar la contraseña
+        const salt = bcryptjs.genSaltSync();
+        resto.password = bcryptjs.hashSync( password, salt );
+    }
+
+    const usuario = await Usuario.findByIdAndUpdate( id, resto );
+
+    res.json(usuario);
 }
+
 
 const usuariosPatch = (req, res = response) => {
     res.json({
@@ -63,12 +66,15 @@ const usuariosPatch = (req, res = response) => {
     });
 }
 
-const usuariosDelete = (req, res = response) => {
-    res.json({
-        msg: 'delete API - usuariosDelete'
-    });
-}
+const usuariosDelete = async(req, res = response) => {
 
+    const { id } = req.params;
+
+    const usuario = await Usuario.findByIdAndUpdate( id, { estado: false } );
+
+
+    res.json(usuario);
+}
 
 
 
