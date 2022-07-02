6d4933bad6699732e562b81f23149e2a24073f7a
cratulas <mauricios.mora@gmail.com>
2022-07-02 16:48:50 +0100

    Video 124 validar contra base de datos

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,4 +1,8 @@
 const { response, request } = require('express');
+const bcryptjs = require('bcryptjs');
+
+
+const Usuario = require('../models/usuario');
 
 
 const usuariosGet = (req = request, res = response) => {
@@ -15,14 +19,31 @@ const usuariosGet = (req = request, res = response) => {
     });
 }
 
-const usuariosPost = (req, res = response) => {
+const usuariosPost = async(req, res = response) => {
 
-    const { nombre, edad } = req.body;
+
+    const { nombre, correo, password, rol } = req.body;
+    const usuario = new Usuario({ nombre, correo, password, rol });
+
+
+    // Verificar si el correo existe
+    const existeEmail = await Usuario.findOne({ correo });
+    if ( existeEmail ) {
+        return res.status(400).json({
+            msg: 'Ese correo ya se encuentra en la DB'
+        })
+    }
+
+    // Encriptar la contraseña
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync( password, salt );
+
+    // Garadar en DB
+    await  usuario.save();
 
     res.json({
         msg: 'post API - usuariosPost',
-        nombre, 
-        edad
+        usuario
     });
 }
 
